a7a0eceb82c2b53ebab04cc0cc3772de2baab9ce
Evgenia Rodionova <jenechkajj353@gmail.com>
2021-04-13 12:36:07 +0300

    new

--- src/main.java
+++ src/main.java
@@ -1,15 +1,15 @@
 public class main {
-    public static void main(String[] args) {
+    public static void main( String[] args ) {
         int balance = 200;
         int bonusScale = 100;
         int balanceReplenishment = 1300;
         int bonus = 0;
-        if (balanceReplenishment >= 1000) {
+        if ( balanceReplenishment >= 1000 ) {
             bonus = balanceReplenishment / bonusScale;
         }
         int totalBalance = balance + balanceReplenishment + bonus;
-        System.out.println("Пополнение " + balanceReplenishment + " руб.");
-        System.out.println("Бонусов начислено " + bonus + " руб.");
-        System.out.println("Баланс " + totalBalance + " руб.");
+        System.out.println( "Пополнение " + balanceReplenishment + " руб." );
+        System.out.println( "Бонусов начислено " + bonus + " руб." );
+        System.out.println( "Баланс " + totalBalance + " руб." );
     }
 }
